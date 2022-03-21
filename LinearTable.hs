4ba55a2373050fba1923ce4e9bf223c0ffc80a1b
Amy de Buitléir <amy@nualeargais.ie>
2022-03-21 12:59:56 +0000

    initial commit

---
+++ LinearTable.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Foldable            (foldl')
+import qualified Data.Text                as T
+import qualified Text.Pandoc              as P
+import           Text.Pandoc.JSON         (toJSONFilter)
+
+main :: IO ()
+main = toJSONFilter formatLinearTable
+
+formatLinearTable :: P.Block -> P.Block
+formatLinearTable x@(P.CodeBlock (_,cs,_) s)
+  | null cs                  = x
+  | head cs == "linear-table" = toTable . splitRows $ T.lines s
+  | otherwise                = x
+formatLinearTable x = x
+
+toTable :: [[T.Text]] -> P.Block
+toTable xss = P.Table attr defaultTableCaption colSpecs
+                    defaultTableHeader [toTableBody xss]
+                    defaultTableFooter
+  where attr = ("",["linear-table"],[])
+        colSpecs = replicate nCols defaultColSpec
+        nCols = maximum $ map length xss
+
+
+toTableBody :: [[T.Text]] -> P.TableBody
+toTableBody = P.TableBody P.nullAttr (P.RowHeadColumns 0) []
+                  . map toTableRow
+
+
+toTableRow :: [T.Text] -> P.Row
+toTableRow = P.Row P.nullAttr . map toCell
+
+toCell :: T.Text -> P.Cell
+toCell = blocksToCell . map removePara . parseBlocks
+
+blocksToCell :: [P.Block] -> P.Cell
+blocksToCell
+  = P.Cell P.nullAttr P.AlignDefault (P.RowSpan 1) (P.ColSpan 1)
+
+removePara :: P.Block -> P.Block
+removePara (P.Para xs) = P.Plain xs
+removePara x           = x
+
+splitRows :: Foldable t => t T.Text -> [[T.Text]]
+splitRows xs = reverse . map reverse $ foldl' splitter [] xs
+
+splitter :: [[T.Text]] -> T.Text -> [[T.Text]]
+splitter [] x | x == ""    = []
+              | otherwise = [[x]]
+splitter accum x | x == ""    = []:accum
+                 | otherwise = (x:(head accum)) : tail accum
+
+
+
+
+readDefaults :: P.ReaderOptions
+readDefaults = P.def { P.readerStandalone = True,
+                       P.readerExtensions = P.pandocExtensions }
+
+parseBlocks :: T.Text -> [P.Block]
+parseBlocks s = bs
+  where (Right (P.Pandoc _ bs)) = P.runPure $ P.readMarkdown readDefaults s
+
+defaultColSpec :: P.ColSpec
+defaultColSpec = (P.AlignDefault, P.ColWidthDefault)
+
+defaultTableCaption :: P.Caption
+defaultTableCaption = P.Caption Nothing []
+
+defaultTableHeader :: P.TableHead
+defaultTableHeader = P.TableHead P.nullAttr []
+
+defaultTableFooter :: P.TableFoot
+defaultTableFooter = P.TableFoot P.nullAttr []
